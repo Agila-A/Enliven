f936ed0eaeab34815af7069987097ab7bcaf9626
bhuvana-pooja <bhuvaneshwarisubramani58@gmail.com>
2025-12-02 13:59:04 +0530

    with no progress bar in dashboard and profile

--- enliven-backend/controllers/profileController.js
+++ enliven-backend/controllers/profileController.js
@@ -1,6 +1,5 @@
 // controllers/profileController.js
 import User from "../models/User.js";
-import Progress from "../models/Progress.js";
 
 // ---------------------------------------------
 // GET /api/profile/me
@@ -51,41 +50,6 @@ export const updateProfile = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Failed to update profile",
-    });
-  }
-};
-
-// ---------------------------------------------
-// GET /api/profile/progress
-// ---------------------------------------------
-export const getCourseProgress = async (req, res) => {
-  try {
-    const userId = req.userId;
-
-    const domain = req.query.domain;
-    const level = req.query.level;
-
-    if (!domain || !level) {
-      return res.status(400).json({
-        success: false,
-        message: "Missing domain or level",
-      });
-    }
-
-    const courseId = `${domain.toLowerCase()}-${level.toLowerCase()}`;
-
-    const progressDoc = await Progress.findOne({ userId, courseId });
-
-    res.json({
-      success: true,
-      progress: progressDoc?.progress || [],
-    });
-
-  } catch (err) {
-    console.error("Get Course Progress Error:", err);
-    res.status(500).json({
-      success: false,
-      message: "Failed to load course progress",
     });
   }
 };
--- enliven-backend/routes/profileRoutes.js
+++ enliven-backend/routes/profileRoutes.js
@@ -3,13 +3,13 @@ import { requireAuth } from "../middleware/authMiddleware.js";
 import {
   getProfile,
   updateProfile,
-  getCourseProgress
+  
 } from "../controllers/profileController.js";
 
 const router = express.Router();
 
 router.get("/me", requireAuth, getProfile);
 router.put("/update", requireAuth, updateProfile);
-router.get("/progress", requireAuth, getCourseProgress);
+
 
 export default router;
